1080da958209e5058d62e0bac794ed4cab91f162
immortal <113749788+immortal-github@users.noreply.github.com>
2023-02-25 10:17:48 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,48 @@
+require('dotenv').config()
+const express = require('express')
+const dbconnection = require('./config/db')
+const cors = require('cors')
+const userRoute = require('./routes/users.route')
+const authentication = require('./middlewares/authentication.middleware')
+const productRoute = require('./routes/products.route')
+const cartRoute = require('./routes/cart.route')
+const UserModel = require('./models/user.model')
+const app = express()
+UserModel
+
+// middlewares
+app.use(cors())
+app.use(express.json())
+// routers
+
+app.get('/',(req,res)=>res.send('homepage Of backend'))
+app.use('/allusers',async(req,res)=>{
+    
+    try {
+        const users = await UserModel.find()
+        if (users) res.json(users)
+        else{
+            res.send('No users found')
+        }
+    } catch (error) {
+        res.json({error : error.message })
+    }
+
+})
+app.use('/users',userRoute)
+app.use('/products',productRoute)
+// protected routes
+app.use(authentication)
+app.use('/cart',cartRoute)
+
+// default route
+
+
+app.listen(process.env.port,()=>{
+    try {
+        dbconnection;
+        console.log("sever runnign at port " +  process.env.port);
+    } catch (error) {
+        console.log({err : error.message})
+    }
+})
